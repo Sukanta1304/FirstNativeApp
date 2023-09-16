a9a114851f8e1086dba0dba9c33b794989e43183
Sukanta1304 <sukantadolai97@gmail.com>
2023-09-16 22:07:00 +0530

    custom tab header added

---
+++ components/CustomTabHeader.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CustomTabHeader = () => {
+  return (
+    <View>
+      <Text>CustomTabHeader</Text>
+    </View>
+  )
+}
+
+export default CustomTabHeader
+
+const styles = StyleSheet.create({})
